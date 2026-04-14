7db4b5efeef096e713ffbf3ab2a74c1512472cb6
Deven Anil Borse <devenborse37@gmail.com>
2026-04-14 11:43:18 +0530

    this code simply gives an idea about compound interest

---
+++ CompoundInterestCalculator.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class CompoundInterestCalculator{
+    public static void main(String[] args) {
+
+
+        // Compound Interest Calculator
+        Scanner sc = new Scanner(System.in);
+
+        double principal, rate, compoundInterest;
+        int timeCompounded, years;
+
+        System.out.print("Enter principal amount: ");
+        principal = sc.nextDouble();
+
+        System.out.print("Enter rate of interest: ");
+        rate = sc.nextDouble() / 100;
+
+        System.out.print("Enter time compounded per year: ");
+        timeCompounded= sc.nextInt();
+
+        System.out.print("Enter number of years: ");
+        years = sc.nextInt();
+
+        compoundInterest = principal * Math.pow(1 + rate / timeCompounded, timeCompounded * years) ;
+
+        System.out.printf("Compound Interest after %d years is $%.2f\n", years, compoundInterest);
+    }
+} 
