fcb73bec41f6d4285667b375ef7d66b6a75cbb98
Deven Anil Borse <devenborse37@gmail.com>
2026-04-26 16:23:02 +0530

    the code to find even numbers

---
+++ EvenNumber.java
@@ -0,0 +1,11 @@
+import java.lang.*;
+
+public class EvenNumber{
+    public static void main(String[] args){
+        for(int i=0;i<=100;i++){
+            if(i%2==0){
+                System.out.println(i);
+            }
+        }
+    }
+}
